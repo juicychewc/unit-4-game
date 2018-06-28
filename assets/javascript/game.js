49b0c4dc57eab69f3dd4715e9e1cd37563d9f670
Sushichan <collin.chew.7188@gmail.com>
2018-06-27 23:06:43 -0700

    working appends poor reference

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,60 @@
 var terranUnits = [
+
     {
         name: "terranMarine",
         displayName: "Marine",
         healthPoints: 200,
-        AttackPower: 10,
-        CounterAttack: 6,
+        attackPower: 10,
+        counterAttack: 6,
         image: "assets/images/marine.jpg"
     },
+
     {
         name: "terranGhost",
         displayName: "Ghost",
         healthPoints: 450,
-        AttackPower: 30,
-        CounterAttack: 30,
+        attackPower: 30,
+        counterAttack: 30,
         image: "assets/images/ghost.jpg"
     },
+
     {
         name: "terranGoliath",
-        displayName:"Goliath",
+        displayName: "Goliath",
         healthPoints: 700,
-        AttackPower: 20,
-        CounterAttack: 15,
+        attackPower: 20,
+        counterAttack: 15,
         image: "assets/images/goliath.jpg"
     },
+
     {
         name: "terranThor",
         displayName: "Thor",
         healthPoints: 2000,
-        AttackPower: 40,
-        CounterAttack: 20,
+        attackPower: 40,
+        counterAttack: 20,
         image: "assets/images/thor.jpg"
     },
+
     {
         name: "terranBattlecruiser",
         displayName: "Battlecruiser",
         healthPoints: 5000,
-        AttackPower: 50,
-        CounterAttack: 50,
+        attackPower: 50,
+        counterAttack: 50,
         image: "assets/images/battlecruiser.jpg"
     }];
 
 var unitTypesDiv = $("#allUnits");
+var isChosen = false;
+var enemyChosen = false;
 
 //This generates a span for each object in the array
 terranUnits.forEach(function (unit) {
     var span = document.createElement("span");
     span.setAttribute("class", "initialClickPointer clickableUnits")
     span.setAttribute("id", unit.name);
+    span.setAttribute("dataset", unit);
     var nameLabel = document.createElement("p");
     nameLabel.textContent = unit.displayName;
     span.appendChild(nameLabel);
@@ -59,23 +67,49 @@ terranUnits.forEach(function (unit) {
     unitTypesDiv.append(span);
 });
 
-//first object click
-$(".initialClickPointer").on("click", function() {
-    this.setAttribute("class", "clickableUnits playerSelected");
-    $(".clickableUnits").removeClass("initialClickPointer");
-    $(".playerSelected").appendTo("#playerChoiceArea");
-    $(".clickableUnits").not(".playerSelected").addClass("enemyUnits");
-    $(".enemyUnits").appendTo("#enemyChoiceArea");
-    $("#instructions").remove();
-    $("#allUnits").remove();
-    $(".initialZoneSpacing").remove();
+//first object selection
+$(".clickableUnits").on("click", function () {
+    if (isChosen === false) {
+        this.setAttribute("class", "clickableUnits playerSelected");
+        isChosen = true;
+        $(".clickableUnits").removeClass("initialClickPointer");
+        $(".playerSelected").appendTo("#playerChoiceArea");
+        $(".clickableUnits").not(".playerSelected").addClass("enemyUnits");
+        $(".enemyUnits").appendTo("#enemyChoiceArea");
+        $("#instructions").remove();
+        $("#allUnits").remove();
+        $(".initialZoneSpacing").remove();
+    }
+
+    //second object selection
+    else {
+        $(".enemyUnits").on("click", function () {
+            if (enemyChosen === false) {
+                this.setAttribute("class", "clickableUnits enemyUnits currentOpponent");
+                enemyChosen === true;
+                var fightButton = document.createElement("button");
+                fightButton.setAttribute("class", "fightButton");
+                fightButton.textContent = "Fight!";
+                $(".currentOpponent").appendTo("#battleZone");
+                $(fightButton).appendTo("#buttonSpot");
+                $(".currentOpponent").off("click");
+            }
+
+            //combat function
+            else {
+                fightButton.on("click", function () {
+                    var chosenId = $(".playerSelected").attr("id");
+                    var chosenUnit = terranUnits[chosenId];
+
+                })
+            }
+        })
+
+    }
 });
 
-//opponent selector
-$(".enemyUnits").on("click", function() {
-    this.setAttribute("class", "clickableUnits enemyUnits currentOpponent");
-    $(".currentOpponent").appendTo("#battleZone");
-    var fightButton = document.createElement("button");
-    button.setAttribute("class", "fightButton");
-    fightButton.prepend("battlezone");
-});
+/*
+function getStronger() {
+    var currentAttack = $(".playerSelected")
+}
+*/
