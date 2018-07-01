40017837c190baa1fa8ff6f912ca622a3aa57356
Sushichan <collin.chew.7188@gmail.com>
2018-07-01 04:17:36 -0700

    1.2 not stronger and too many buttons

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -51,9 +51,12 @@ var isChosen = false;
 var enemyChosen = false;
 var chosenId;
 var enemyId;
+var chosenHp;
+var currentAttack = 0;
+var currentEnemyHp;
+var enemiesDefeated = 0;
 
 //This generates a span for each object in the array
-// terranUnits.forEach(function (unit) {
 for (var key in terranUnits) {
     var span = document.createElement("span");
     span.setAttribute("class", "initialClickPointer clickableUnits");
@@ -67,11 +70,12 @@ for (var key in terranUnits) {
     img.src = terranUnits[key].image;
     span.appendChild(img);
     var hp = document.createElement("p");
+    hp.setAttribute("class", "remainingHp");
     hp.textContent = terranUnits[key].healthPoints;
     span.appendChild(hp);
     unitTypesDiv.append(span);
+    isChosen === false;
 };
-
 
 //first object selection
 $(".clickableUnits").on("click", function () {
@@ -85,6 +89,9 @@ $(".clickableUnits").on("click", function () {
         $("#instructions").remove();
         $("#allUnits").remove();
         $(".initialZoneSpacing").remove();
+        chosenId = $(".playerSelected").attr("id");
+        chosenUnit = terranUnits[chosenId];
+        chosenHp = chosenUnit.healthPoints;
     }
 
     //second object selection
@@ -93,40 +100,96 @@ $(".clickableUnits").on("click", function () {
             if (enemyChosen === false) {
                 this.setAttribute("class", "clickableUnits enemyUnits currentOpponent");
                 enemyChosen === true;
-                var fightButton = document.createElement("button");
-                fightButton.setAttribute("class", "fightButton");
-                fightButton.textContent = "Fight!";
-                $(".currentOpponent").appendTo("#battleZone");
-                $(fightButton).appendTo("#buttonSpot");
-                $(".currentOpponent").off("click");
+                if ($("#buttonSpot").length > 0) {
+                    var fightButton = document.createElement("button");
+                    fightButton.setAttribute("class", "fightButton");
+                    fightButton.textContent = "Fight!";
+                    $(".currentOpponent").appendTo("#battleZone");
+                    $("#buttonSpot").append(fightButton);
+                    $(".currentOpponent").off("click");
+                }
             }
         })
     }
 });
 
+function gameOver() {
+    location.reload();
+};
+
+$("#combat").on("click", "#resetButton", function () {
+    gameOver();
+    this.remove();
+});
+
 //combat function
 $("#buttonSpot").on("click", ".fightButton", function () {
     chosenId = $(".playerSelected").attr("id");
+    enemyId = $(".currentOpponent").attr("id");
     chosenUnit = terranUnits[chosenId];
-    if (terranUnits.indexOf(chosenId) >= 0) {
+    if ($(this).length != -1) {
         var chosenAttack = chosenUnit.attackPower;
-        var chosenHp = chosenUnit.healthPoints;
-        var enemyId = $(".currentOpponent").attr("id");
         var currentOpponent = terranUnits[enemyId];
         var currentCounter = currentOpponent.counterAttack;
-        var currentEnemyHp = currentOpponent.healthPoints;
+        currentEnemyHp = currentOpponent.healthPoints;
         function thatHurt() {
             chosenHp = chosenHp - currentCounter;
         };
         function getStronger() {
-            var currentAttack = currentAttack + chosenAttack;
-        }
+            currentAttack = currentAttack + chosenAttack;
+        };
         function takeThis() {
             currentEnemyHp = currentEnemyHp - currentAttack;
-        }
+        };
+        function combatText() {
+            var combatDiv = $("#combat");
+            var damageOut = $("<p></p>");
+            var damageIn = $("<p></p>");
+            damageOut.text("You attacked " + currentOpponent.displayName + " for " + currentAttack);
+            damageIn.text(currentOpponent.displayName + " attacked you back for " + currentCounter);
+            var textArea = $("<div></div>");
+            combatDiv.empty();
+            textArea.append(damageOut, damageIn);
+            combatDiv.append(textArea);
+        };
+        function damageResult() {
+            var playerRemainingHp = $(".playerSelected p.remainingHp");
+            var enemyRemainingHp = $(".currentOpponent p.remainingHp");
+            playerRemainingHp.text(chosenHp);
+            enemyRemainingHp.text(currentEnemyHp);
+        };
+        function mayIHaveAnother() {
+            if (enemiesDefeated < 4) {
+            var resolve = $("#combat");
+            resolveMessage = $("<p></p>");
+            resolveMessage.text("You defeated " + currentOpponent.displayName + "!");
+            resolve.empty();
+            resolve.append(resolveMessage);
+            $(".currentOpponent").remove();
+            $(".fightButton").remove();
+            enemyChosen === false;
+            enemiesDefeated + 1;
+            }
+            else {
+                resolve.empty();
+                resolveMessage.text("You defeated everyone!");
+                resolve.append(resolveMessage);
+            }
+        };
         getStronger();
         thatHurt();
         takeThis();
+        combatText();
+        damageResult();
+        if ((currentEnemyHp <= 0) && (chosenHp > 0)) {
+            mayIHaveAnother();
+        }
+        if ((chosenHp <= 0) && (currentEnemyHp > 0)) {
+            var resetButton = document.createElement("button");
+            resetButton.setAttribute("id", "resetButton");
+            resetButton.textContent = ("Respawn");
+            $(resetButton).appendTo("#combat");
+        }
     }
 });
 
